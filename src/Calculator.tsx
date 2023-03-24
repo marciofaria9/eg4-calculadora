9f2fb73087ba4b3836db71049b4c73a7287eb4bb
Jose Augusto de Oliveira Pins <joliveirapins.jaop@gmail.com>
2023-03-24 14:16:28 -0300

    Inserindo os botões

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,10 +1,48 @@
-import { Container } from "./styles"
+import { ButtonContainer, ButtonContainerDown, Input } from "./components/styles"
+import { Button, ButtonGrenn, ButtonRed, ContainerCalc, Header } from "./styles"
+import seta from './assets/seta.svg';
 
 
 export const Calculator = () => {
-    return (
-        <Container>
+  return (
+    <>
+      <ContainerCalc>
+        <Header>
+          Calculadora Univas
+        </Header>
 
-        </Container>
-    )
+        <Input>
+
+        </Input>
+        <ButtonContainer>
+
+            <Button> + </Button>
+            <Button> - </Button>
+            <Button> X </Button>
+            <Button> / </Button>
+            
+            <Button> 7 </Button>
+            <Button> 8 </Button>
+            <Button> 9 </Button>
+            <ButtonRed><img src={seta} alt='seta' /></ButtonRed>
+
+            <Button> 4 </Button>
+            <Button> 5 </Button>
+            <Button> 6 </Button>
+            <ButtonGrenn> = </ButtonGrenn>
+
+            <Button> 1 </Button>
+            <Button> 2 </Button>
+            <Button> 3 </Button>
+            <Button /> 
+
+            <Button> 0 </Button>
+            <Button> , </Button>
+            <ButtonRed> CE </ButtonRed>
+            <Button /> 
+        </ButtonContainer>
+
+      </ContainerCalc>
+    </>
+  )
 }
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,8 +1,92 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  border: 1px solid black;
-  border-radius: 8px;
-  padding: 320px 300px 320px 160px;
-  background-color: black;
+export type Props = {
+  color?: string;
+  bg?: string;
+  bdbox?: string;
+  gc?: string;
+}
+
+export const ContainerCalc = styled.div`
+  max-width: 500px;
+  min-width: 300px;
+  margin: 0 auto;
+  user-select: none;
+  padding: 10px 10px 10px 10px;
+
+  @media screen and (min-width: 450px) {
+    min-width: 360px;
+  }
+`
+
+export const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Header = styled(Flex)`
+  justify-content: space-between;
+  color: var(--black-text);
+  font-size: 1.2rem;
+`
+
+export const Button = styled(Flex)<Props>`
+  border-radius: 0.3rem;
+  padding: 25px;
+  font-size: 2.2rem;
+  opacity: 0.8;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  grid-column: ${(props) => (props.gc ? props.gc : 'auto')};
+  background-color: ${(props) => 
+    props.color ? props.color : '#606060' };
+  color: ${(props) => 
+    props.color ? props.color : '#fff' };
+  box-shadow: 0 3px 0
+    ${(props) => (props.bdbox ? props.bdbox : 'var(--key-shadow-orange') };
+  &:hover {
+    opacity: -1;
+  }
+`
+
+export const ButtonRed = styled(Flex)<Props>`
+  border-radius: 0.3rem;
+  padding: 25px;
+  font-size: 2.0rem;
+  opacity: 0.8;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  grid-column: ${(props) => (props.gc ? props.gc : 'auto')};
+  background-color: ${(props) => 
+    props.color ? props.color : '#BD5757' };
+  color: ${(props) => 
+    props.color ? props.color : '#fff' };
+  box-shadow: 0 3px 0
+    ${(props) => (props.bdbox ? props.bdbox : 'var(--key-shadow-orange') };
+  &:hover {
+    opacity: -1;
+  }
+`
+
+export const ButtonGrenn = styled(Flex)<Props>`
+  border-radius: 0.3rem;
+  padding: 25px;
+  font-size: 2.0rem;
+  opacity: 0.8;
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  grid-column: ${(props) => (props.gc ? props.gc : 'auto')};
+  background-color: ${(props) => 
+    props.color ? props.color : '#437B3E' };
+  color: ${(props) => 
+    props.color ? props.color : '#fff' };
+  box-shadow: 0 3px 0
+    ${(props) => (props.bdbox ? props.bdbox : 'var(--key-shadow-orange') };
+  &:hover {
+    opacity: -1;
+  }
 `
